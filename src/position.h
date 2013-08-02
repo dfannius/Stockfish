3b5cea69096f49d235387dc6b575b3fbbff4146d
Dan Schmidt <dfan@dfan.org>
2013-08-02 19:22:18 -0400

    Fix bad merge

--- src/position.h
+++ src/position.h
@@ -193,13 +193,8 @@ private:
   // Helper functions
   void do_castle(Square kfrom, Square kto, Square rfrom, Square rto);
   Bitboard hidden_checkers(Square ksq, Color c) const;
-<<<<<<< HEAD
-  void remove_piece(Square s, Color c, PieceType pt);
-  void add_piece(Square s, Color c, PieceType pt);
-=======
   void put_piece(Square s, Color c, PieceType pt);
   void remove_piece(Square s, Color c, PieceType pt);
->>>>>>> upstream/master
   void move_piece(Square from, Square to, Color c, PieceType pt);
 
   // Computing hash keys from scratch (for initialization and debugging)
@@ -421,25 +416,17 @@ inline Thread* Position::this_thread() const {
   return thisThread;
 }
 
-<<<<<<< HEAD
-inline void Position::add_piece(Square s, Color c, PieceType pt) {
-=======
 inline void Position::put_piece(Square s, Color c, PieceType pt) {
 
   board[s] = make_piece(c, pt);
   byTypeBB[ALL_PIECES] |= s;
   byTypeBB[pt] |= s;
   byColorBB[c] |= s;
->>>>>>> upstream/master
   index[s] = pieceCount[c][pt]++;
   pieceList[c][pt][index[s]] = s;
 }
 
 inline void Position::move_piece(Square from, Square to, Color c, PieceType pt) {
-<<<<<<< HEAD
-  // index[from] is not updated and becomes stale. This works as long
-  // as index[] is accessed just by known occupied squares.
-=======
 
   // index[from] is not updated and becomes stale. This works as long
   // as index[] is accessed just by known occupied squares.
@@ -449,23 +436,10 @@ inline void Position::move_piece(Square from, Square to, Color c, PieceType pt) 
   byColorBB[c] ^= from_to_bb;
   board[from] = NO_PIECE;
   board[to] = make_piece(c, pt);
->>>>>>> upstream/master
   index[to] = index[from];
   pieceList[c][pt][index[to]] = to;
 }
 
-<<<<<<< HEAD
-/// Position::remove_piece() updates pieceList[] and index[] to
-/// indicate that the specified piece has been removed from the board,
-/// by moving the last element of pieceList into the newly empty slot.
-
-inline void Position::remove_piece(Square s, Color c, PieceType pt)
-{
-  // WARNING: This is not a reversible operation. If we remove a piece in
-  // do_move and then replace it in undo_move() we will put it at the end of the
-  // list and not in its original place, it means index[] and pieceList[] are
-  // not guaranteed to be invariant to a do_move() + undo_move() sequence.
-=======
 inline void Position::remove_piece(Square s, Color c, PieceType pt) {
 
   // WARNING: This is not a reversible operation. If we remove a piece in
@@ -476,7 +450,6 @@ inline void Position::remove_piece(Square s, Color c, PieceType pt) {
   byTypeBB[pt] ^= s;
   byColorBB[c] ^= s;
   /* board[s] = NO_PIECE; */ // Not needed, will be overwritten by capturing
->>>>>>> upstream/master
   Square lastSquare = pieceList[c][pt][--pieceCount[c][pt]];
   index[lastSquare] = index[s];
   pieceList[c][pt][index[lastSquare]] = lastSquare;
